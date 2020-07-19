31f9d6b6984f4e101c715b3b861a7c2b601717e7
salah <email@example.com>
2020-07-19 18:03:58 +0200

    .

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ fore Running this Project
  1. Install npm packages using 'npm install' 
   
  2. Run " node server.js"
+ 3. open the webpage in a web browser: http://localhost:3000/
--- app/routes/article.routes.js
+++ app/routes/article.routes.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
     const articles = require('../controllers/article.controller.js');
 
-    // Create a new Note
+    // Create a new Article
     app.post('/articles/Add', articles.create);
 
     // Retrieve all articles
     app.get('/articles/List', articles.findAll);
 
-    // Retrieve a single Note with noteId
+    // Retrieve a single Articlewith ArticleId
     app.get('/articles/GetById/:ArticleId', articles.findOne);
 
-    // Update a Note with noteId
+    // Update a Articlewith ArticleId
     app.put('/articles/Update/:ArticleId', articles.update);
 
-    // Delete a Note with noteId
+    // Delete a Articlewith ArticleId
     app.delete('/articles/Delete/:ArticleId', articles.delete);
 }
